c60cbece057d5d04f7931035886de5c267c68db3
macfisher <macfisher01@gmail.com>
2015-10-02 21:42:15 -0600

    Added addListener func to Dispatcher class

--- event2.php
+++ event2.php
@@ -24,6 +24,45 @@ class Dispatcher {
 		$this->events = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8" standalone="yes"?><events></events>');
 	}
 	
-	public function addListener($callback, $event) {}
+	public function addListener($callback, $event) {
+		$return = false;
+		
+		if (is_callable($callback) === true) {
+			if (is_array($callback)) {
+				$id = md5(get_class($callback[0]) . json_encode($callback));
+			} else {
+				$id = md5(json_encode($callback));
+			}
+			
+			$matches =& $this->_getEvent($event);
+			
+			foreach($matches as $match) {
+				$status = $match->xpath('./listener[@id=\'' . $id . '\']');
+				$status = ($status === false || count($status) === 0) ? false : true;
+				
+				if ($status === false) {
+					$listener = $match->addChild('listener');
+					$listener['id'] = $id;
+					
+					unset($listener);
+				}
+			}
+			
+			if (!isset($this->callback[$id])) {
+				$this->callback[$id] =& $callback;
+			}
+			
+			$return = true;
+		}
+		
+		return $return;
+	}
+	
 	public function notify(Event &$event) {}
 }
+
+
+
+
+
+
